cbc9ec800cee5f34c40e1722c1be0b12eb8f28cc
Kossa <kossaaudi84@gmail.com>
2026-09-14 10:13:33 +0700

    Implement FilamentUser contract agar panel bisa diakses di production

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -15,10 +17,15 @@ use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, HasRoles, LogsActivity, Notifiable;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->roles()->exists();
+    }
 
     public function getActivitylogOptions(): LogOptions
     {
